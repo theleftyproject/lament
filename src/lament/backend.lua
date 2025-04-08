b3580f4ea3cc19428917132a8c767184bebedeb0
Sparkles-Laurel <sparkles@gayest.systems>
2025-04-08 14:51:16 +0300

    update copyright notices for 2025

--- src/lament/backend.lua
+++ src/lament/backend.lua
@@ -1,6 +1,6 @@
 -- backend.lua - defines backend API functions
 --
---     Copyright (C) 2024  Kıvılcım Defne Öztürk
+--     Copyright (C) 2024-2025  Kıvılcım Defne Öztürk
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
--- src/lament/loader.lua
+++ src/lament/loader.lua
@@ -1,6 +1,6 @@
 -- loader.lua - loads LAMENT config modules
 --
---     Copyright (C) 2024  Kıvılcım Defne Öztürk
+--     Copyright (C) 2024-2025  Kıvılcım Defne Öztürk
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
