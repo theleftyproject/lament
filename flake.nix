70037ab36928df70c9834f268a0112afe316b647
Makuru <Makuru@Makuru.org>
2025-05-07 15:03:19 +0200

    Ran formatter for more consistend look.

--- flake.nix
+++ flake.nix
@@ -15,38 +15,49 @@
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 {
-    description = "Simple bidirectional configuration system";
+  description = "Simple bidirectional configuration system";
 
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    inputs.flake-utils.url = "github:numtide/flake-utils";
-    inputs.flake-utils.inputs.systems.follows = "nixpkgs";
-    # inputs.asdf.url = "github:asdf-vm/asdf";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.systems.follows = "nixpkgs";
+  # inputs.asdf.url = "github:asdf-vm/asdf";
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = import nixpkgs { inherit system; };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-                lua = pkgs.luajit;
-                luarocks = pkgs.luarocks.override { inherit lua ;};
-            in {
-                formatter = pkgs.nixfmt-tree;
-                devShells.default = pkgs.mkShell {
-                    name = "luajit-dev-env";
+        lua = pkgs.luajit;
+        luarocks = pkgs.luarocks.override { inherit lua; };
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+        devShells.default = pkgs.mkShell {
+          name = "luajit-dev-env";
 
-                    nativeBuildInputs = [
-                        pkgs.just
-                        lua
-                        luarocks
-                    ];
-                };
+          nativeBuildInputs = [
+            pkgs.just
+            lua
+            luarocks
+          ];
+        };
 
-                packages.default = pkgs.runCommand "build-with-just" {
-                    buildInputs = [ pkgs.just ];
-                } ''
-                    just build
-                    touch $out
-                ''
-            ;}
-        );
+        packages.default =
+          pkgs.runCommand "build-with-just"
+            {
+              buildInputs = [ pkgs.just ];
+            }
+            ''
+              just build
+              touch $out
+            '';
+      }
+    );
 }
