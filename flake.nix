a511c55238ad5e73e4e507932d1241185529a899
Sparkles-Laurel <sparkles@gayest.systems>
2025-05-07 12:01:09 +0300

    implement more build tools

--- flake.nix
+++ flake.nix
@@ -2,30 +2,39 @@
     description = "Simple bidirectional configuration system";
 
     inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+    inputs.flake-utils.inputs.systems.follows = "systems";
     inputs.asdf.url = "github:asdf-vm/asdf";
+
     imports.luajit = {
         url = "github:NixOS/nixpkgs/nixos-24.11";
         inputs = { self, nixpkgs };
     };
 
-    outputs = { self, nixpkgs, asdf, luajit, ... }: let
-        pkgs = import nixpkgs { system = builtins.currentSystem ; };
-    in {
-        packages.${builtins.currentSystem} = rec {
-            lament = pkgs.stdenv.mkDerivation rec {
-                pname = "lament";
-                version = "dev-1";
-                src = ./.;
+    outputs = { self, nixpkgs, asdf, luajit, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
 
-                nativeBuildInputs = [
-                    asdf,
-                    just
-                ];
+                lua = pkgs.luajit;
+                luarocks = pkgs.luarocks.override { inherit lua ;};
+            in {
+                devShells.default = pkgs.mkShell {
+                    name = "luajit-dev-env";
 
-                buildInputs = [
+                    nativeBuildInputs = [
+                        pkgs.just
+                        lua
+                        luarocks
+                    ];
+                };
 
-                ]
+                packages.default = pkgs.runCommand "build-with-just" {
+                    buildInputs = [ pkgs.just ];
+                } ''
+                    just build
+                    touch $out
+                ''
             }
-        }
-    }
+        );
 }
