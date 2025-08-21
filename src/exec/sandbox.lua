79934173e1de9d1833e08b8f0c2517725261b659
Kıvılcım İ. <sparkles@gayest.systems>
2025-08-21 16:04:34 +0300

    add checks to sandbox

--- src/exec/sandbox.lua
+++ src/exec/sandbox.lua
@@ -15,7 +15,10 @@ function lament.sandbox.apply_module(module_path)
       if not f then
          error(err)
       end
-      return f.apply()
+      if f["apply"] == nil then
+         return f.apply()
+      end
+      return error("this backend lacks an apply field")
 end
 
 function lament.sandbox.recalibrate_module(module_path)
@@ -23,5 +26,8 @@ function lament.sandbox.recalibrate_module(module_path)
       if not f then
          error(err)
       end
-      return f.recalibrate()
+      if f["recalibrate"] == nil then
+         return f.recalibrate()
+      end
+      return error("this backend lacks a recalibrate field")
 end
