41890e92918d0cc6fb48c0bbb2f59e30596958fd
Kıvılcım İ. <sparkles@gayest.systems>
2025-08-22 11:24:11 +0300

    remove ambiguous sandbox dependency

--- src/exec/sandbox.lua
+++ src/exec/sandbox.lua
@@ -15,7 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-local sandbox = require("sandbox")
 local lament = require("lament")
 lament.sandbox = {}
 
