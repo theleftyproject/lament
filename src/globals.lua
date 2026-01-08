21a5eff47d608e666ce56447b506742e5a211c6d
Sparkles-Laurel <sparkles@gayest.systems>
2026-01-08 19:33:53 +0300

    separate constants from globals

--- src/globals.lua
+++ src/globals.lua
@@ -1,6 +1,6 @@
--- src/const.lua - Constants
+-- src/globals.lua - Global variables
 --
---    Copyright (C) 2025 - 2026 Kıvılcım İpek Defne Öztürk
+--    Copyright (C) 2026 Kıvılcım İpek Defne Öztürk
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -14,19 +14,22 @@
 --
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 local lament = require("lament")
-lament.const = {}
-lament.const.config = {}
-lament.const.config.DEFAULT_BACKEND_PATH = {
-   "/usr/share/lament/backends"
-}
-lament.const.config.DEFAULT_AUTOCONF_PATH = {
-   "/etc/lament/lament.conf.lua",
-   "/etc/lament/self.conf.lua",
-   "/etc/lament/auto.conf.lua"
-}
+
+--- The global system configuration object,
+--- This represents the state S in the configuration theory
+lament.globals._SYSCONF = {}
+
+--- The effective configuration object
+--- This represents the state E in the configuration theory
+lament.globals._EFFSTAT = {}
+
+--- Configuration modules.
+--- A backend may specify more than one module
+lament.globals._MODULES = {}
 
 return {
-   --- Constants
-   const = lament.const,
+   --- Global variables
+   globals = lament.globals
 }
